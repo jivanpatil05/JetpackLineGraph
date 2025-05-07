7a2f321712264a0cf7c6df14b3bc4b39e1bf2f2d
Jivan <jeevan.p@reapmind.com>
2025-05-07 18:13:59 +0530

    first comm

--- app/src/main/java/com/example/jetpacklinegraph/MainActivity.kt
+++ app/src/main/java/com/example/jetpacklinegraph/MainActivity.kt
@@ -4,19 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.example.jetpacklinegraph.ui.theme.JetpackLineGraphTheme
-import com.example.jetpacklinegraphlibrary.model.DataPoint
-import com.example.jetpacklinegraphlibrary.ui.LineGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
