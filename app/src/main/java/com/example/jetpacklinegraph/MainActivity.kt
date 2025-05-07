36e049525cfe19371ebd6bbbd33f8ed3b91ab142
Jivan <jeevan.p@reapmind.com>
2025-05-07 18:03:08 +0530

    first comm

--- app/src/main/java/com/example/jetpacklinegraph/MainActivity.kt
+++ app/src/main/java/com/example/jetpacklinegraph/MainActivity.kt
@@ -24,35 +24,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackLineGraphTheme {
-                /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding).fillMaxSize()) {
-                        val dataPoints = mutableListOf(
-                            DataPoint("Jan", 0f),
-                            DataPoint("Feb", 0f),
-                            DataPoint("Mar", 1200f),
-                            DataPoint("Apr", 2000f),
-                            DataPoint("May", 1500f),
-                            DataPoint("Jun", 1600f),
-                        )
-                        LineGraph(
-                            data = dataPoints,
-                            maxY = 2500f,
-                            nationalAverage = 1500f,
-                            graphColor = Color.Blue,
-                            averageLineColor = Color.Green,
-                            gridColor = Color.Gray,
-                            labelTextColor = Color.Black,
-                            labelTextSizeSp = 14.sp,
-                            strokeWidth = 5f,
-                            circleRadius = 5f,
-                            labelStep = 500,
-                            spacing = 80f,
-                            cardBackgroundColor = Color.White,
-                            showCircleOnPoints = true,
-                            showVerticalLine = true
-                        )
-                    }
-                }*/
+
             }
         }
     }
